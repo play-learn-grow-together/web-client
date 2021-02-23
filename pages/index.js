66cd58f14f364cbea8cdd9e425ff057c180832e7
Pyunghyuk Yoo <yoophi@gmail.com>
2021-02-23 18:58:27 +0900

    :sparkles: ADD: tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 export default function Home() {
   return (
-    <div>empty index</div>
+    <>
+    <div className="bg-gray-50 mx-4 my-4 shadow rounded-lg">
+      <div className="px-4 py-5 sm:p-6">
+        <div>empty index</div>
+      </div>
+    </div>
+    </>
   )
 }
